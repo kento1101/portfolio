50ccfdf0a784c8a2d90bebfec75f362882f9efea
kento1101 <teshimaaaaa1101@gmail.com>
2021-07-25 09:44:29 +0000

    非同期通信化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,19 @@
 class CommentsController < ApplicationController
-  
+
 def create
-  style = Style.find(params[:style_id])
-  comment = current_user.comments.new(comment_params)
-  comment.style_id= style.id
-  comment.save
-  redirect_to style_path(style)
+  @style = Style.find(params[:style_id])
+  @comment = current_user.comments.new(comment_params)
+  @comment.style_id = @style.id
+  @comment.save
+
 end
-  
-  
-  
+
+
 def destroy
-  Comment.find_by(id: params[:id], style_id: params[:style_id]).destroy
-  redirect_to style_path(params[:style_id])
+   @style = Style.find(params[:style_id])
+  @comment=Comment.find_by(id: params[:id], style_id: params[:style_id])
+  @comment.destroy
+
 end
 
 
@@ -20,7 +21,7 @@ private
 
 def comment_params
   params.require(:comment).permit(:body)
-end  
-  
-  
+end
+
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,6 +3,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
    belongs_to :style
 
-   attachment :image
 
 end
