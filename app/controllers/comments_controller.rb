183d3866c4fd6325614793566b94904bc970473e
kento1101 <teshimaaaaa1101@gmail.com>
2021-08-26 07:14:13 +0000

    修正:follow機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ def create
   @comment = current_user.comments.new(comment_params)
   @comment.style_id = @style.id
   @comment.save
+  @style_comments = @style.comments.page(params[:page]).per(5)
 end
 
 
@@ -12,6 +13,7 @@ def destroy
   @style = Style.find(params[:style_id])
   @comment=Comment.find_by(id: params[:id], style_id: params[:style_id])
   @comment.destroy
+  @style_comments = @style.comments.page(params[:page]).per(5)
 end
 
 
